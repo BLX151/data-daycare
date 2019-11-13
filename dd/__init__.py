9bd78c2e97254e439e03f93559efe81297f26af6
BLX151 <tylermckay10@gmail.com>
2019-11-13 15:29:01 -0400

    fixed daycare blueprint issue
    motherfucking data type was wrong beacuase python doesn't giev a shit about what type of data it is

--- dd/__init__.py
+++ dd/__init__.py
@@ -1,26 +1,35 @@
 # import flask class Flask which contatins all of the internal 
 # web server logic
 from flask import Flask, render_template, url_for, request
-from .db import db
+from dd.db import db
 from dd.daycare.daycare import daycare
 
-# Instantiate a copy of the Flask class called app
-app = Flask(__name__)
-app.register_blueprint(daycare)
-
+app = Flask(__name__) # Instantiate a copy of the Flask class called app
+app.register_blueprint(daycare) # Register the daycare blueprint into the app
 
 # Our base domain page, @app.route creates a webpage at
 # www.ourdomain.com/<routename> which anyone can access
 @app.route('/')
-def default(): # Function linked to the routed webpaged
+# The home route
+@app.route('/home')
+def home(): # Function linked to the routed webpaged
+    return render_template('home.html', nearby = db.getXElements(5))
     # Return the html file to be displayed on the routed page
-    return render_template('home.html', nearby = db.getXElements(5), recent = db.getXElements(3))
+    # nearby variable will be availble to access inside the html file
 
-# Routes /about as a webpage, returns the about.html template
+# The map route
+@app.route('/map')
+def map():
+    return render_template("map.html", database = db.getDatabase())
+    # database variable will be accessable from within map.html
+
+# The directory route
+@app.route('/directory')
+def directory():
+    return render_template("directory.html", database = db.getDatabase())
+    # database variable will be accessable from within directory.html
+
+# The about route
 @app.route('/about')
 def about():
     return render_template('about.html')
-
-
-
-
