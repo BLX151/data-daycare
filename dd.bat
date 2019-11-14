f68197d8920a3de260584029a6f11347f5ecd3ee
TCM <tylermckay10@gmail.com>
2019-11-14 16:47:55 -0400

    Initial nearby home functionality

--- dd.bat
+++ dd.bat
@@ -1,26 +1,25 @@
 @ECHO OFF
 
-ECHO %1
-
 IF %1==on GOTO ON
 IF %1==off GOTO OFF
 GOTO ERR
 
 :ON 
+@ECHO OFF
 CALL %cd%\pyenv\scripts\activate
 SET FLASK_APP=dd
 SET FLASK_ENV=development
-CLS
-ECHO DD Activated
+ECHO == DD Activated ==
 GOTO DONE
 
 :OFF
+@ECHO OFF
 CALL %cd%\pyenv\scripts\deactivate
-CLS
-ECHO DD Deactivated
+ECHO  == DD Deactivated ==
 GOTO DONE
 
 :ERR
+@ECHO OFF
 ECHO "Format: dd on; dd off"
 GOTO DONE
 
