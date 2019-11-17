3d53b0e679c689b7c2c125990d1b1f8eaf765a36
TCM <tylermckay10@gmail.com>
2019-11-17 16:05:47 -0400

    Filter by distance now works
    Home page will show the 5 closest daycares to your position

--- dd/__init__.py
+++ dd/__init__.py
@@ -3,41 +3,23 @@
 from flask import Flask, render_template, url_for, request
 from dd.db import db
 from dd.daycare.daycare import daycare
-import requests# geopy.location
+import requests
 
 app = Flask(__name__) # Instantiate a copy of the Flask class called app
 app.register_blueprint(daycare) # Register the daycare blueprint into the app
 
-# def getDistance(coords1, coords2):
-#     return geopy.distance.vincenty(coords1, coords2).km
-
-# def findNearby(user_lat, user_long):
-#     database = db.getDatabase()
-#     nearby = [
-#         {"index": -1, "distance": 99999},
-#         {"index": -1, "distance": 99999},
-#         {"index": -1, "distance": 99999},
-#         {"index": -1, "distance": 99999},
-#         {"index": -1, "distance": 99999},
-#     ]
-#     for daycare in database:
-#         distance = getDistance( (user_lat,user_long), (daycare.location.latitude,darecare.location.latitude) )
-#         if (distance < nearby[4]):
-#             for (i = nearby.length; i > 0; i -= 1)
-        
-#     return nearby
+def getUserGeoData():
+    user_ip = requests.get('https://get.geojs.io/v1/ip.json').json()['ip']
+    return requests.get('https://get.geojs.io/v1/ip/geo/' + user_ip + '.json').json()
 
 # Our base domain page, @app.route creates a webpage at
 # www.ourdomain.com/<routename> which anyone can access
 @app.route('/')
 # The home route
 @app.route('/home')
-def home(): # Function linked to the routed webpaged
-    # user_ip = requests.get('https://get.geojs.io/v1/ip.json').json()['ip']
-    # user_geoData = requests.get('https://get.geojs.io/v1/ip/geo/' + user_ip + '.json').json()
-    # nearby = findNearby(user_geoData['latitude'], user_geoData['longitude'])
-
-    return render_template('home.html', nearby = db.getXElements(5))
+def home():
+    nearby = db.getByDistance(25, 5, getUserGeoData())
+    return render_template('home.html', nearby = nearby)
     # Return the html file to be displayed on the routed page
     # nearby variable will be availble to access inside the html file
 
--- dd/db/db.py
+++ dd/db/db.py
@@ -1,4 +1,5 @@
-import requests, json # Allows the fetching and storing of the API calls
+import requests, json, sys # Allows the fetching and storing of the API calls
+from math import radians, cos, sin, sqrt, atan2
 
 # === Update the database from the Nova Scotia open data API ===
 def updateDatabase(daycares):
@@ -41,6 +42,68 @@ def indexify():
         daycare["index"] = index # Add a new key 'index' which has a unique value for each daycare
         index += 1 # Increment the index for the next daycare
     return database # Return the indexed database
+
+# Calculates the distance between two poistions, returns distance
+def getDistance(lat1, lon1, lat2, lon2):
+    R = 6373.0  # approximate radius of earth in km
+    lat1 = radians(float(lat1)) # Must convert
+    lat2 = radians(float(lat2))
+    lon1 = radians(float(lon1)) # these all to
+    lon2 = radians(float(lon2)) #floats then radians
+    dlon = lon2 - lon1 # Get difference between longitudes
+    dlat = lat2 - lat1 # Get difference between latitudes
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    # ^^^^^^^^^^^^^^ DO FANCY MATHS IDC ABOUT ^^^^^^^^^^^^^^^^^^^^^^
+    return R * c
+
+# Method to sort inputted array of dictionaries with "distance" key by distance
+# THIS IS A PEICE OF SHIT, don't touch
+def sortByDistance(nearby):
+    notSorted = True
+    noChange = True
+    # print("LENGTH = " + str(len(nearby)), file=sys.stderr)
+    i = 0
+    while (notSorted):
+        noChange = True
+        # print("INDEX" + str(i), file=sys.stderr)
+        if (i+1 == len(nearby) and noChange):
+            # print("SORTED", file=sys.stderr)
+            break
+        if (i+1 >= len(nearby) and notSorted):
+            i = 0
+            # print("NOT SORTED", file=sys.stderr)
+            continue
+        if (int(nearby[i]["distance"]) <= int(nearby[i+1]["distance"])):
+            i += 1
+            # print("SKIP", file=sys.stderr)
+            continue
+        if (int(nearby[i]["distance"]) >= int(nearby[i+1]["distance"])):
+            temp = nearby[i+1]
+            nearby[i+1] = nearby[i]
+            nearby[i] = temp
+            i += 1
+            noChange = False
+            # print("SWAP", file=sys.stderr)
+            continue
+    return nearby
+
+# Controller function to get the
+def getByDistance(maxDistance, amount, user_geoData):
+    userlat = user_geoData["latitude"]
+    userlon = user_geoData["longitude"]
+    database = getDatabase()
+    nearby = []
+    for daycare in database:
+        if ('location' in daycare):
+            distance = getDistance(userlat, userlon, daycare["location"]["latitude"], daycare["location"]["longitude"])
+            # print(distance, file=sys.stderr)
+            if (distance < maxDistance):
+                daycare["distance"] = distance
+                nearby.append(daycare)
+    # print("SORTING NOW", file=sys.stderr)
+    nearby = sortByDistance(nearby)
+    return nearby[:amount]
 
 # === Fixes the casing from the original dataset. It doesn't display how we want it to. ===
 # DO NOT TOUCH, THIS WORKS AS EXPECTED AND I DON'T REMEMBER HOW I DID IT
