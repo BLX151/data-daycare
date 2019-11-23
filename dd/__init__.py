bd68f722ea5bb898c29e4b8e430289e967a96f9b
BLX151 <tylermckay10@gmail.com>
2019-11-23 17:20:59 -0400

    SLAMMED OUT ALL THE UPDATES

--- dd/__init__.py
+++ dd/__init__.py
@@ -27,9 +27,42 @@ def home():
     # nearby variable will be availble to access inside the html file
 
 # The map route
-@app.route('/map')
+@app.route('/map', methods = ["POST", "GET"])
 def map():
-    return render_template("map.html", database = db.getDatabase())
+    filters = {}
+    if (request.form.get('county') != "noSelection" and request.form.get('county') != None ):
+        filters['county'] = request.form.get('county')
+    if (request.form.get('city') != "noSelection" and request.form.get('city') != None):
+        filters['city'] = request.form.get('city')
+    if (request.form.get('min_capacity') != "" and request.form.get('min_capacity') != None ):
+        filters['min_capacity'] = int(request.form.get('min_capacity'))
+    if (request.form.get('max_capacity') != "" and request.form.get('max_capacity') != None ):
+        filters['max_capacity'] = int(request.form.get('max_capacity'))
+    if (request.form.get('age_infant') == "on"):
+        filters['age_infant'] = "Yes"
+    if (request.form.get('age_toddler') == "on"):
+        filters['age_toddler'] = "Yes"
+    if (request.form.get('age_preschool') == "on"):
+        filters['age_preschool'] = "Yes"
+    if (request.form.get('age_school_age') == "on"):
+        filters['age_school_age'] = "Yes"
+    if (request.form.get('prog_full_day') == "on"):
+        filters['prog_full_day'] = "Yes"
+    if (request.form.get('prog_part_day') == "on"):
+        filters['prog_part_day'] = "Yes"
+    if (request.form.get('annualInspection') == "on"):
+        filters['annual_inspection'] = "Yes"
+    if (request.form.get('unannouncedInspection') == "on"):
+        filters['annual_unannounced_inspection'] = "Yes"
+
+    # return filters
+
+    if not filters:
+        indexes = db.getDatabase()
+    else:
+        indexes = db.filterDaycares(filters)
+
+    return render_template("map.html", database = indexes)
     # database variable will be accessable from within map.html
 
 @app.route('/directory', methods = ["POST", "GET"])
